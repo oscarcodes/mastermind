a1ad3d51c120439eef2e9d387d13bc1ba7bd3c82
oscarcodes <ocr.abstracta@gmail.com>
2019-12-11 12:05:40 +0100

    20191211_implementada_jugada_aleatoria_atzar

--- mastermind.cc
+++ mastermind.cc
@@ -46,7 +46,7 @@ char demanar_opcio(){
 	while (opcio!='I' and opcio!='J'){
 	//Inv: Les opcions entrades fins ara son !='J' i !='I'
 		cout<<endl<<"Mode de joc incorrecte.";
-		cout<<"Mode Interactiu (I) o Joc de proves (J) : ";
+		cout<<endl<<"Mode Interactiu (I) o Joc de proves (J) : ";
 		cin>>opcio;
 	}
 	cout<< endl;
@@ -58,17 +58,22 @@ bool correcte(const vector<int>& v){
 	//Pre: v es un vector d'enters no buit
 	//Post: Retorna true si la mida del vector es 4, tots els elements de v son diferents, 		//	i els elements es troben a [1..8]
 	//	En cas contrari retorna false
+
 	bool trobat=false;
 	if (v.size()==4) {
 		unsigned int i=0;
 		while (i<v.size() and not trobat){
 			//Inv: els elements [0..i-1] no estan repetits
+			// i a més a més l'element i no està a [0..j-1]
+			// ni es un 0, ni es un 9
 			unsigned int j=i+1;
+			if ((v[i]==0) or (v[i]==9)) {
+				trobat=true;
+			}
+
 			while (j<v.size() and not trobat) {
 				//Inv: els elements [0..i-1] no estan repetits
-				// i a més a més l'element i no està a [0..j-1]
-				// ni es un 0, ni es un 9
-				if ((v[i]==v[j]) or (v[i]==0) or (v[i]==9) or (v[i]==0)) trobat=true;
+				if ((v[i]==v[j])) trobat=true;
 				else ++j;
 			}
 			if (not trobat) ++i;
@@ -100,27 +105,47 @@ string genera_resposta (const vector<int>& secret, const vector<int>& intent) {
 }
 
 vector<int> separar (int x) {
-//Pre : x es possitiu x=X
-//Post : Retorna un vector amb els digits de x
-vector<int> v;
-while (x>0){
-	// Invariant: tots els digits que estan a X i no a x estan dins del vector v
-	v.push_back(x%10);
-	x=x/10;
+	//Pre : x es possitiu x=X
+	//Post : Retorna un vector amb els digits de x
+	vector<int> v;
+	while (x>0){
+		// Invariant: tots els digits que estan a X i no a x estan dins del vector v
+		v.push_back(x%10);
+		x=x/10;
+	}
+
+	//for (unsigned int i=0; i<v.size(); ++i) cout<<v[i]<<" ";
+	cout<<endl;
+	//Girem el contingut
+	unsigned int n=v.size(), j;
+	for(unsigned int i=0; i<n/2; ++i){
+		// Invariant: hem girat els numeros que estan entre 0 i i-1
+		j= n-i-1;
+		int aux=v[i];
+		v[i]=v[j];
+		v[j]=aux;
+	}
+return v;
 }
 
-//for (unsigned int i=0; i<v.size(); ++i) cout<<v[i]<<" ";
-	cout<<endl;
-//Girem el contingut
-unsigned int n=v.size(), j;
-for(unsigned int i=0; i<n/2; ++i){
-	// Invariant: hem girat els numeros que estan entre 0 i i-1
-	j= n-i-1;
-	int aux=v[i];
-	v[i]=v[j];
-	v[j]=aux;
-}
-return v;
+bool tirada_aleatoria(vector<int> &tirada){
+	
+	vector <bool> nombre_posat(8,false);
+	int digit;
+	digit=1+rand()%8;
+	tirada.push_back(digit);
+	nombre_posat[digit]=true;
+	for (int i=1;i<4;i++) {
+ 
+		digit=1+rand()%8;
+		while (nombre_posat[digit]) digit=1+rand()%8;
+		tirada.push_back(digit);
+		nombre_posat[digit]=true;
+		
+	}
+
+	cout << tirada[0]<< tirada[1]<< tirada[2]<< tirada[3]<<endl;
+	return true;
 }
 
 
@@ -171,8 +196,15 @@ int main() {
 		int codi_jugador_b;
 		vector<int> v_codi_jugador_b;
 
-		cin>> codi_jugador_b;
-		v_codi_jugador_b=separar(codi_jugador_b);
+		if (joc=='M') {
+			cin>> codi_jugador_b;
+			v_codi_jugador_b=separar(codi_jugador_b);
+		}
+		else
+		{
+			tirada_aleatoria(v_codi_jugador_b);
+
+		}
 
 		while (!(correcte(v_codi_jugador_b))) {
 			cout<<"intent incorrecte."<<endl;
@@ -205,8 +237,8 @@ int main() {
 			}
 		}
 
-		if (v_codi_jugador_b[0]==9 and v_codi_jugador_b[1]==9  
-			and v_codi_jugador_b[2]==9 and v_codi_jugador_b[3]==9) fin_juego=true;
+		//if (v_codi_jugador_b[0]==9 and v_codi_jugador_b[1]==9  
+		//	and v_codi_jugador_b[2]==9 and v_codi_jugador_b[3]==9) fin_juego=true;
 		if (resposta=="XXXX") {
 			fin_juego=true;
 			guanyat=true;
